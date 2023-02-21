50dca558ab717a9e9470ba1438b916453b67ae12
GabrielSousaJS <gabriels.santosx@gmail.coom>
2023-02-20 22:46:07 -0300

    Fix: correção na classe ResourceExceptionHandler

--- attornatus/src/main/java/com/attornatus/attornatus/resources/exceptions/ResourceExceptionHandler.java
+++ attornatus/src/main/java/com/attornatus/attornatus/resources/exceptions/ResourceExceptionHandler.java
@@ -26,18 +26,6 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(status).body(err);
     }
 
-    @ExceptionHandler(DatabaseException.class)
-    public ResponseEntity<StandardError> databaseException(DatabaseException e, HttpServletRequest request) {
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        StandardError err = new StandardError();
-        err.setTimestamp(Instant.now());
-        err.setStatus(status.value());
-        err.setError("Database exception");
-        err.setMessage(e.getMessage());
-        err.setPath(request.getRequestURI());
-        return ResponseEntity.status(status).body(err);
-    }
-
     @ExceptionHandler(TwoMainAddressException.class)
     public ResponseEntity<StandardError> twoMainAddressException(TwoMainAddressException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_ACCEPTABLE;
